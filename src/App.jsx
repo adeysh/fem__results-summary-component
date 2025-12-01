9d3456a58f4235343e50eefe6e636a84654429c0
Adesh Katiya <adeshkatiya27@gmail.com>
2025-12-01 22:57:13 +0530

    added attribution footer and removed unnecessary html

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import ContinueButton from "./components/ContinueButton";
 import ResultCard from "./components/ResultCard";
 import SummaryCard from "./components/SummaryCard";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <div className="flex flex-col md:flex-row justify-center bg-white md:rounded-3xl">
-            <ResultCard />
-            <div className="p-10 flex flex-col gap-6">
-                <SummaryCard />
-                <ContinueButton />
+        <>
+            <div className="flex flex-col md:flex-row justify-center bg-white md:rounded-3xl">
+                <ResultCard />
+                <div className="p-10 flex flex-col gap-6">
+                    <SummaryCard />
+                    <ContinueButton />
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div class="attribution">
+            Challenge by{" "}
+            <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+            >
+                Frontend Mentor
+            </a>
+            . Coded by <a href="https://www.frontendmentor.io/profile/adeysh">Adesh Katiya</a>.
+        </div>
+    );
+};
+
+export default Footer;
